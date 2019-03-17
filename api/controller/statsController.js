b71f7dcecada4346762eea164983f43fc28a1489
mesamtimmar1 <syed.mesam1@gmail.com>
2019-03-17 18:36:24 -0230

    statsController: Get matches list by acountId

--- api/controller/statsController.js
+++ api/controller/statsController.js
@@ -2,13 +2,22 @@ const leagueJs = require('../../lib/leagueJs');
 
 process.env.LEAGUE_API_PLATFORM_ID = 'na';
 const leaguePlatformId = process.env.LEAGUE_API_PLATFORM_ID;
-
+const numOfMatchesRequired = 10;
 
 const getLeagueStatsBySummonerName = (req, res) => {
     const summonerName = req.params.summonerName;
     leagueJs.Summoner
-        .gettingByName(summonerName )
-        .then(summonerData => res.json(summonerData))
+        .gettingByName(summonerName)
+        .then(summonerData => {
+            const matchListOptions = {
+                endIndex: numOfMatchesRequired 
+            };
+            leagueJs.Match
+            .gettingListByAccount(summonerData.accountId, leaguePlatformId, options=matchListOptions)
+            .then(matchesData => {
+                res.json(matchesData);
+            });
+        })
         .catch(err => res.send(404));
 }
 
