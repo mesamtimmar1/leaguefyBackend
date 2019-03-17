0cdb57c7185dd9334e5107eb2ee5ab2a5ad3eef6
mesamtimmar1 <syed.mesam1@gmail.com>
2019-03-17 18:02:02 -0230

    statsController: Get basic summonerdata by name

--- api/controller/statsController.js
+++ api/controller/statsController.js
@@ -1,9 +1,15 @@
+const leagueJs = require('../../lib/leagueJs');
+
+process.env.LEAGUE_API_PLATFORM_ID = 'na';
+const leaguePlatformId = process.env.LEAGUE_API_PLATFORM_ID;
+
+
 const getLeagueStatsBySummonerName = (req, res) => {
-    console.log('Request processed');
-    const result = {
-        result: 'Request processed from controller'
-    }
-    res.json(result);
+    const summonerName = req.params.summonerName;
+    leagueJs.Summoner
+        .gettingByName(summonerName )
+        .then(summonerData => res.json(summonerData))
+        .catch(err => res.send(404));
 }
 
 module.exports.getLeagueStatsBySummonerName = getLeagueStatsBySummonerName;
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,6 @@
 const stats = require('../controller/statsController');
 
 module.exports =  (app) => {
-    app.route('/api/lol/summoner/stats')
+    app.route('/api/lol/summoner/stats/:summonerName')
         .get(stats.getLeagueStatsBySummonerName);
 }
